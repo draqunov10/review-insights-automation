9119c210c2110220aca5c66ba4bb54d8d927b6db
draqunov10 <ledrhoniel@gmail.com>
2025-09-06 18:53:50 +0800

    feat: complete data processing

--- algorithms.py
+++ algorithms.py
@@ -1,6 +1,4 @@
 import json
-import os
-
 
 # Turn multiple JSON lines into an array of dicts
 def parse_json_lines(file_path: str = "./cache_data/LDV_places.jsonl") -> list[dict]:
@@ -21,10 +19,13 @@ def filter_dealerships(places: list[dict]) -> list[dict]:
     def has_dealer(categories, place):
         if categories is None: raise KeyError(f"Missing 'categories' in place: {place}")
         return any("dealer" in cat.lower() for cat in categories)
+    if not places: return []
     return [place for place in places if has_dealer(place.get("categories"), place)]
 
 # Reduce the number of reviews by filtering selected month
 def filter_reviews_by_month(reviews: list[dict], month: int | str) -> list[dict]:
+    if not reviews: return []
+    
     # Accept month as int or str, convert to int
     try: month_int = int(month)
     except Exception: raise ValueError("Month must be an integer from 1 to 12.")
@@ -34,7 +35,6 @@ def filter_reviews_by_month(reviews: list[dict], month: int | str) -> list[dict]
     for review in reviews:
         review_date = review.get("When")
         if review_date:
-            
             # Expect format 'YYYY-M-D' or 'YYYY-MM-DD', handle both
             # Skip invalid/problematic dates
             parts = review_date.split("-")
@@ -47,26 +47,35 @@ def filter_reviews_by_month(reviews: list[dict], month: int | str) -> list[dict]
             
     return filtered
 
-# Only take the necessary data: rating and review itself, throw error if keys don't exist
-def filter_reviews_keys(reviews: list[dict], keys: list[str] = ["Rating", "Description"]) -> list[dict]:
-    filtered = []
-    for review in reviews:
-        
-        missing_keys = [key for key in keys if key not in review]
-        if missing_keys: raise KeyError(f"Missing keys {missing_keys} in review: {review}")
-        
-        filtered_review = {key: review[key] for key in keys}
-        filtered.append(filtered_review)
-        
-    return filtered
+# Only take the necessary data, throw error if keys don't exist
+def filter_keys(d: dict, keys: list[str]) -> dict:
+    missing_keys = [key for key in keys if key not in d]
+    if missing_keys: raise KeyError(f"Missing keys {missing_keys} in review: {d}")
+
+    return {key: d[key] for key in keys}
 
 # Return an array of dealerships json ready to feed by batch
-def process_input():
+def process_input(month: str | int) -> list[dict]:
     raw_ldv_places = parse_json_lines()
     filtered_ldv_dealerships = filter_dealerships(raw_ldv_places)
-    for dealership in filtered_ldv_dealerships:
-        dealership["user_reviews_extended"] = filter_reviews_by_month(dealership.get("user_reviews_extended"), "2")
-    return filtered_ldv_dealerships
+    filtered_ldv_dealerships_keys = [filter_keys(dealership, [
+        "title",
+        "review_count",
+        "review_rating",
+        "reviews_per_rating",
+        "user_reviews_extended"
+    ]) for dealership in filtered_ldv_dealerships]
+
+    for dealership in filtered_ldv_dealerships_keys: 
+        dealership["user_reviews_extended"] = filter_reviews_by_month(dealership.get("user_reviews_extended"), month)
+        dealership["user_reviews_extended"] = [
+            filter_keys(review, [
+            "Rating",
+            "Description"
+            ]) for review in dealership["user_reviews_extended"]
+        ]
+    
+    return filtered_ldv_dealerships_keys
 
 def process_summary():
     pass
@@ -77,12 +86,6 @@ def make_pdf():
 
 # Test usage by running this
 if __name__ == "__main__":
-    json_objects = parse_json_lines()
-    print(f"Parsed {len(json_objects)} JSON objects.")
-
-    for obj in json_objects:
-        print(obj["title"])
-        # for review in obj['user_reviews_extended']:
-        #     print(review['Name'], end=', ')
-        # print("===================\n")
-            
+    for filtered_dealership in process_input(8):
+        print(filtered_dealership)
+        print(f"{'=' * 20}\n")
